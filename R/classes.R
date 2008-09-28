f545ff537c18fae5325880ac89c61cdf37f5ec71
m.robinson <m.robinson@bc3139a8-67e5-0310-9ffc-ced21a209358>
2008-09-28 03:38:09 +0000

    added a few checks on DGEList objects
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/edgeR@34020 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/classes.R
+++ R/classes.R
@@ -33,6 +33,14 @@ setMethod("show","deDGEList",
 
 DGEList <- function(data=matrix(0), lib.size=integer(0), group=factor(), ...)
 {
+  if (ncol(data) != length(lib.size))
+    stop("Length of 'lib.size' must equal number of columns in 'data'")
+  if (ncol(data) != length(group))
+    stop("Length of 'group' must equal number of columns in 'data'")
+  if (!is.factor(group))
+    group<-as.factor(group)
+  if (length(levels(group)) != 2)
+    warning("Implementation is only for 2 groups at this stage")
   new("DGEList",list(data=data, lib.size=lib.size, group=group, ...))
 }
 
