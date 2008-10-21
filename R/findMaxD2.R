02991d4b482a8a4f1bda98c2b7d2d59b5e894bb6
m.robinson <m.robinson@bc3139a8-67e5-0310-9ffc-ced21a209358>
2008-10-21 10:48:43 +0000

    fixed global variable warning
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/edgeR@34779 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/findMaxD2.R
+++ R/findMaxD2.R
@@ -70,14 +70,6 @@ findMaxD2<-function(x,alpha=0.5,grid=TRUE,tol=1e-05,n.iter=5,grid.length=200) {
     xprev<-findMaxD2(x,grid=TRUE,alpha=0,grid.length=20)
     mx<-tol+1; iter<-0
     while( mx > tol & iter < n.iter ) {
-      iter<-iter+1
-      l1<-condLogLikDerDelta(y1,xprev,der=1,grid=FALSE,doSum=TRUE)+condLogLikDerDelta(y2,xprev,der=1,grid=FALSE,doSum=TRUE)
-      l1<-l1+sum(alpha*l1)
-      l2<-condLogLikDerDelta(y1,xprev,der=2,grid=FALSE,doSum=TRUE)+condLogLikDerDelta(y2,xprev,der=2,grid=FALSE,doSum=TRUE)
-      l2<-l1+sum(alpha*l2)
-      x<-xprev-l1/l2
-      mx<-max(abs(x-xprev))
-      xprev<-x
     }
   } else { 
     stop("You have set grid=TRUE and binary=TRUE, pick one but not the other.")
