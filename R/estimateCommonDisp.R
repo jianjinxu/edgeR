0ff0e4e8a2137a0ac42cc636f8a7564cea6e250b
m.robinson <m.robinson@bc3139a8-67e5-0310-9ffc-ced21a209358>
2009-10-13 11:41:09 +0000

    added estimateCommonDisp.R
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/edgeR@42342 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/estimateCommonDisp.R
@@ -0,0 +1,30 @@
+estimateCommonDisp <- function(object,tol=1e-06) 
+#	Do two iterations of calculating pseudodata and estimating common dispersion, first one uses Poisson
+{
+	if (!is(object,"DGEList"))
+		stop("Currently supports DGEList objects")
+	group<-object$samples$group
+	levs.group<-levels(group)
+	for(i in seq_len(2)) {
+		if(i==1) disp <- 0 else disp <- common.dispersion$dispersion
+		q2q.out<-equalizeLibSizes(object,disp=disp,null.hypothesis=FALSE)
+		pseudo.obj<-new("DGEList",list(counts=q2q.out$pseudo, samples=object$samples))
+		common.dispersion <- .estimateCommonDisp(pseudo.obj, tol=tol)
+	}
+	new("DGEList",list(samples=object$samples, common.dispersion=common.dispersion$dispersion, counts=object$counts, pseudo.alt=q2q.out$pseudo, conc=q2q.out$conc, common.lib.size=q2q.out$N))
+}
+
+.estimateCommonDisp <- function(object, tol=1e-06)
+# Estimates the common dispersion (using conditional maximum likelihood)
+# for fixed counts (y), assuming library sizes are equal
+# Calculated on the delta = phi/(1+phi) scale,
+# returns dispersion on the phi and the delta scale
+# Now uses optimize instead of a grid search or Newton-Raphson
+{
+	nrows<-nrow(object$counts)
+	levs.group<-levels(object$samples$group)
+	y<-splitIntoGroups(object)
+	delta <- optimize(commonCondLogLikDerDelta, interval=c(1e-4,100/(100+1)), tol=tol, maximum=TRUE, y=y, der=0, doSum=FALSE)
+	delta <- delta$maximum
+	list(dispersion=delta/(1-delta), dispersion.delta=delta)
+}
