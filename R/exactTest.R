027e17d1d3b8334fd343492d3f41be81047ef7f5
m.robinson <m.robinson@bc3139a8-67e5-0310-9ffc-ced21a209358>
2009-10-21 10:31:35 +0000

    small modification of how 'pair' is printed to screen
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/edgeR@42535 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/exactTest.R
+++ R/exactTest.R
@@ -3,19 +3,18 @@ exactTest<-function(object,pair=NULL, common.disp=TRUE)
 # Calculates exact p-values for the differential expression levels of tags in the two groups being compared
 {
 	if (!is(object,"DGEList")) stop("Currently only supports DGEList objects as the object argument.")
-	levs.group<-levels(object$samples$group)
+        group <- object$samples$group
+	levs.group<-levels(group)
 	if(is.null(pair)) {
-		pair<-levels(as.factor(c(levs.group[1],levs.group[2])))
+		pair<-levels(as.factor(levs.group[1:2]))
 	} else if(!is.factor(pair)) {
 		pair<-as.factor(pair)
-		#pair<-levels(as.factor(pair))
 	}
-	if( sum(pair[1]==levs.group)==0 | sum(pair[2]==levs.group)==0 ) 
+	if( sum(pair[1]==levs.group)==0 | sum(pair[2]==levs.group)==0 )
 		stop("At least one element of given pair is not a group.\n Groups are: ",paste(levs.group, collapse=" "),"\n")
-	cat("Comparison of groups: ",pair[2],"-",pair[1],"\n")
-	
-	this.pair<-( pair[1]==object$samples$group | pair[2]==object$samples$group )
-	group.pair <- factor(as.vector(object$samples$group[this.pair]))
+	cat("Comparison of groups: ",paste(pair[2:1],collapse=" - "),"\n")
+	this.pair<-( pair[1]==group | pair[2]==group )
+	group.pair <- factor(as.vector(group[this.pair]))
 	levs.pair<-levels(group.pair)
 	obj.pair <- DGEList(counts=object$counts[,this.pair], group=group.pair, lib.size=object$samples$lib.size[this.pair])
 	if(common.disp)	
@@ -27,7 +26,7 @@ exactTest<-function(object,pair=NULL, common.disp=TRUE)
 	q2q.pair <- equalizeLibSizes(obj.pair,disp=dispersion,null.hypothesis=TRUE)
 	mus <- object$common.lib.size*q2q.pair$conc$conc.common
 	exact.pvals<-exactTestNB(q2q.pair$pseudo,obj.pair$samples$group,pair,mus,r=1/dispersion)
-	
+
 	logConc<-(log2(q2q.pair$conc$conc.group[,pair[1]==levs.pair])+log2(q2q.pair$conc$conc.group[,pair[2]==levs.pair]))/2
 	logFC<-log2(q2q.pair$conc$conc.group[,pair[2]==levs.pair]/q2q.pair$conc$conc.group[,pair[1]==levs.pair])
 	de.out<-data.frame(logConc=logConc, logFC=logFC, p.value=exact.pvals)
