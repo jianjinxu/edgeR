ce2b1900ae7d8ab9c8a5ab0180f99f07928d8051
d.mccarthy <d.mccarthy@bc3139a8-67e5-0310-9ffc-ced21a209358>
2010-03-12 01:42:21 +0000

    'disp' argument added to exactTest() so that a user can specify value(s) for the dispersion when doing the exact test; Andy's plotMDS2() added to the package with documentation.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/edgeR@45186 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/exactTest.R
+++ R/exactTest.R
@@ -1,27 +1,46 @@
-exactTest<-function(object,pair=NULL, common.disp=TRUE)
+exactTest<-function(object, pair=NULL, disp=NULL, common.disp=TRUE)
 # Written by by Davis McCarthy, September 2009, last modified 15 December 2009
 # Calculates exact p-values for the differential expression levels of tags in the two groups being compared
 {
-	if (!is(object,"DGEList")) stop("Currently only supports DGEList objects as the object argument.")
-	if (is.null(object$common.dispersion) && is.null(object$tagwise.dispersion)) stop("Value(s) for the dispersion parameter must be specified. Try running estimateCommonDisp() and/or estimateTagwiseDisp() before exactTest().")
+	if( !is(object,"DGEList") )
+          stop("Currently only supports DGEList objects as the object argument.")
+	if( is.null(disp) && is.null(object$common.dispersion) && is.null(object$tagwise.dispersion) )
+          stop("Value(s) for the dispersion parameter must be specified. Try running estimateCommonDisp() and/or estimateTagwiseDisp() before exactTest().")
 	object$samples$group <- as.factor(object$samples$group)
-	levs.group<-levels(object$samples$group)
-	if (is.null(pair))
-		pair <- levs.group[1:2]
+	levs.group <- levels(object$samples$group)
+        if( is.null(rownames(object$counts)) )
+          rownames(object$counts) <- paste("tag",1:nrow(object$counts),sep=".")
+	if( is.null(pair) )
+          pair <- levs.group[1:2]
 	if( !all(pair %in% levs.group) )
-		stop("At least one element of given pair is not a group.\n Groups are: ", paste(levs.group, collapse=" "), "\n")
+          stop("At least one element of given pair is not a group.\n Groups are: ", paste(levs.group, collapse=" "), "\n")
 	if(length(pair)!=2) stop("Pair must be of length 2.")
 	if(is.numeric(pair)) pair <- levels(object$samples$group)[pair]
 	else pair <- as.character(pair)	
-	this.pair<-( object$samples$group %in% pair )
+	this.pair <- ( object$samples$group %in% pair )
 	cat("Comparison of groups: ",as.vector(pair[2]),"-",as.vector(pair[1]),"\n")
 	group.pair <- factor(as.vector(object$samples$group[this.pair]))
-	levs.pair<-levels(group.pair)
+	levs.pair <- levels(group.pair)
 	obj.pair <- DGEList(counts=object$counts[,this.pair], group=group.pair, lib.size=object$samples$lib.size[this.pair])
-	if(common.disp)	
-		dispersion <- object$common.dispersion 
-	else 
-		dispersion <- object$tagwise.dispersion
+        if(!is.null(disp)) {
+          if( length(disp)!=1 && length(disp)!=nrow(object$counts) )
+            stop("Disp provided by user must have length either 1 or the number of tags in the DGEList object.\n")
+          if( length(disp)==nrow(object$counts) ) {
+            tags.retained <- rownames(object$counts) %in% rownames(obj.pair$counts)
+            if( sum(tags.retained)!=nrow(object$counts) )
+              disp <- disp[tags.retained]
+          }
+          if(any(disp==0))
+            disp[disp==0] <- 1e-06
+          dispersion <- disp
+        } else {
+            if(common.disp)	
+              dispersion <- object$common.dispersion 
+            else {
+              tags.retained <- rownames(object$counts) %in% rownames(obj.pair$counts)
+              dispersion <- object$tagwise.dispersion[tags.retained]
+            }
+          }
 	q2q.pair <- equalizeLibSizes(obj.pair,disp=dispersion,null.hypothesis=TRUE)
 	mus <- object$common.lib.size*q2q.pair$conc$conc.common
 	y<-splitIntoGroupsPseudo(q2q.pair$pseudo,group.pair,pair)
---
+++ R/plotMDS2.R
@@ -0,0 +1,28 @@
+plotMDS2 <- function (x, top=500, col=NULL, cex=1, dim.plot=c(1, 2), ndim=max(dim.plot), ...) 
+{
+    if (is.matrix(x)){
+          x <- DGEList(counts = x, group = c(rep("1", ncol(x))))
+          colnames(x$counts) <- 1:dim(x$counts)[2]
+    }
+    if (!is(x, "DGEList"))
+          stop("Currently supports DGEList object and matrix")
+    mx <- as.matrix(x$counts)
+    labels <- colnames(x$counts)
+    nprobes <- nrow(mx)
+    nsamples <- ncol(mx)
+    if (ndim < 2) 
+        stop("Need at least two dim.plot")
+    if (nsamples < ndim) 
+        stop("Too few samples")
+    dd <- matrix(0, nrow = nsamples, ncol = nsamples)
+    twd <- estimateTagwiseDisp(estimateCommonDisp(x), prior.n = 10, grid.length = 500)
+    o <- order(twd$tagwise.dispersion, decreasing = TRUE)[1:min(nprobes,top)]
+    subdata <- x$counts[o,]
+    for (i in 2:(nsamples)) 
+    	for (j in 1:(i - 1))
+            dd[i, j] = sqrt(estimateCommonDisp(DGEList(counts=subdata[,c(i,j)], group=c("1","1")))$common.dispersion)
+    a1 <- cmdscale(as.dist(dd), k = ndim)
+    plot(a1[, dim.plot[1]], a1[, dim.plot[2]], type = "n", xlab = paste("Dimension",dim.plot[1]), ylab = paste("Dimension", dim.plot[2]), ... )
+    text(a1[, dim.plot[1]], a1[, dim.plot[2]], labels = labels, col=col, cex=cex)
+    return(invisible(dd))
+}
