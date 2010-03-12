471c0d6347c6ef78a837d91eb1c41f472cdd292b
d.mccarthy <d.mccarthy@bc3139a8-67e5-0310-9ffc-ced21a209358>
2010-03-12 01:33:26 +0000

    Bug fix in estimateSmoothing(); Newton-Raphson estimation now used for calculation of tau0.2 in the new function .odls(), which replaces the optimize() call to tau2.0.objective(); 'disp' argument added to exactTest() so that a user can specify value(s) for the dispersion when doing the exact test; Andy's plotMDS2() added to the package with documentation.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/edgeR@45184 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/estimateSmoothing.R
+++ R/estimateSmoothing.R
@@ -1,5 +1,5 @@
 estimateSmoothing<-function(object,verbose=TRUE) 
-# Written by Mark Robinson, edited by Davis McCarthy, February 2009
+# Written by Mark Robinson, edited by Davis McCarthy, March 2010
 # A function to estimate a value for alpha, the weight for the weighted conditional log-likelihood, using approximate empirical Bayes methods
 # Smoothing parameter is reported as "prior n", that is the weight given to the common conditional log-likelihood in terms of the number of observations that weight is equivalent to. E.g. having prior n = 10 means that the common conditional log-likelihood is given weight equivalent to 10 tag observations.
 {
@@ -11,13 +11,40 @@ estimateSmoothing<-function(object,verbose=TRUE)
 	scores<-0
 	for(i in 1:length(levs.group)) {
 		if ( sum(group==levs.group[i]) > 1) {
-			scores<-scores+condLogLikDerDelta(object$pseudo.alt[,group==levs.group[i]],d,der=1,grid=FALSE, doSum=TRUE)
+			scores<-scores+condLogLikDerDelta(object$pseudo.alt[,group==levs.group[i]],d,der=1,grid=FALSE, doSum=FALSE)
 		}
 	}
 	exp.inf<-approx.expected.info(object,d,object$pseudo.alt)
 	names(exp.inf)<-rownames(object$counts)
-	sigma2.0.est<-optimize(tau2.0.objective,c(0,500),info.g=exp.inf,score.g=scores)$min
+	sigma2.0.est<-.odls(scores.g=scores,info.g=exp.inf)
 	alpha<-1/(sigma2.0.est*nrow(object$counts)*mean(exp.inf))
 	prior.n <- alpha*nrow(object$counts)
 	prior.n
+}
+
+
+
+.odls <- function(scores.g, info.g) {
+# Written by Davis McCarthy, March 2010. A function using Newton-Raphson's method to find the root of the function giving the overdispersion of the likelihood scores
+  tau02 <- 0
+  iter <- 0
+   if (mean(scores.g^2/info.g) < 1) {
+     warning("Estimate of overdispersion of likelihood scores is strictly zero.\n Returning min val close to zero (1e-08).")
+     tau02 <- 1e-08
+     return(tau02)
+    }
+  repeat {
+    iter <- iter + 1
+    Q <- sum(scores.g^2/info.g/(1+info.g*tau02) - 1)
+    derQ <- -sum(scores.g^2/(1+info.g*tau02)^2)
+    dif <- Q/derQ
+    tau02  <- tau02 - dif
+    if (abs(dif/(sqrt(tau02)+.01)) < 1e-06)
+      break
+    if (iter > 50) {
+      warning("Iteration limit exceeded")
+      break
+    }
+  }
+  tau02
 }
