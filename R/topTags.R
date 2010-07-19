c1f662c27519dc037971a46bd2bc64e5a0eb8044
smyth@wehi.edu.au <smyth@wehi.edu.au@bc3139a8-67e5-0310-9ffc-ced21a209358>
2010-07-19 04:43:57 +0000

    topTags will now use logFC as secondary ranking when p-values are identical.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/edgeR@48190 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/topTags.R
+++ R/topTags.R
@@ -1,17 +1,21 @@
-topTags<-function(object,n=10,adjust.method="BH",sort.by="p.value") 
-# Written by Mark Robinson, edited by Davis McCarthy, September 2009
-# A function that displays a summary table of the n most differentially expressed tags, judged on p-value
+topTags <- function(object,n=10,adjust.method="BH",sort.by="p.value") 
+#	Summary table of the n most differentially expressed tags
+#	Mark Robinson, Davis McCarthy, Gordon Smyth
+#	Created September 2008.  Last modified 19 July 2010.
 {
-	
-	if(sort.by=="logFC")  o<-order(abs(object$table$logFC),decreasing=TRUE) 
-	else o<-order(object$table$p.value)
+	sort.by <- match.arg(sort.by,c("p.value","logFC"))
+	alfc <-abs(object$table$logFC)
+	switch(sort.by,
+		"logFC" = {o <- order(alfc,decreasing=TRUE)},
+		"p.value" = {o <- order(object$table$p.value,1/alfc)}
+	)
 	adj.p.val <- p.adjust(object$table$p.value,adjust.method)
 	chosen <- o[1:min(nrow(object$table),n)]
-	tab<-data.frame(logConc=object$table$logConc[chosen], logFC=object$table$logFC[chosen], p.value=object$table$p.value[chosen] ,adj.p.val=adj.p.val[chosen])
+	tab <- data.frame(logConc=object$table$logConc[chosen], logFC=object$table$logFC[chosen], p.value <- object$table$p.value[chosen] ,adj.p.val=adj.p.val[chosen])
 	if(!is.null(object$genes)){
 		tab <- cbind(object$genes[chosen,,drop=FALSE], tab) # Assumes that object$genes is a data.frame
 	}
-	rownames(tab)<-rownames(object$table[chosen,])
-	colnames(tab)<-c(colnames(object$genes), "logConc","logFC","PValue","FDR")
+	rownames(tab) <- rownames(object$table[chosen,])
+	colnames(tab) <- c(colnames(object$genes), "logConc","logFC","PValue","FDR")
 	new("TopTags",list(table=tab, adjust.method=as.character(adjust.method),comparison=as.character(object$comparison)))
 }
