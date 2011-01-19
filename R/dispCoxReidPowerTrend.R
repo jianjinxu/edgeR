bd83a6a6f8f207796445123742b2716e7c8d8385
smyth@wehi.edu.au <smyth@wehi.edu.au@bc3139a8-67e5-0310-9ffc-ced21a209358>
2011-01-19 06:17:25 +0000

    Update to dispCoxReidPowerTrend.R.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/edgeR@51965 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/dispCoxReidPowerTrend.R
+++ R/dispCoxReidPowerTrend.R
@@ -1,12 +1,14 @@
-dispCoxReidPowerTrend <- function(y, design, lib.size, abundance=NULL, trace=0)
-#	Estimate trend dispersion=a*mean^b, log(dispersion)
+dispCoxReidPowerTrend <- function(y, design, lib.size, offset=NULL, abundance=NULL, method.optim="Nelder-Mead", trace=0)
+#	Estimate trend dispersion=a*mean^b
 #	Gordon Smyth
-#	16 Dec 2010.  Last modified 17 Dec 2010.
+#	16 Dec 2010.  Last modified 19 Jan 2011.
 {
 	y <- as.matrix(y)
 	nlibs <- ncol(y)
 	ntags <- nrow(y)
-	offset <- matrix(log(lib.size),ntags,nlibs,byrow=TRUE)
+	if(is.null(offset)) offset <- matrix(log(lib.size),ntags,nlibs,byrow=TRUE)
+	method.optim <- match.args(method.optim, c("Nelder-Mead", "BFGS"))
+	
 	if(is.null(abundance)) abundance <- mglmOneGroup(y,offset=offset)
 	
 	fun <- function(par,y,design,offset,abundance) {
@@ -15,7 +17,7 @@ dispCoxReidPowerTrend <- function(y, design, lib.size, abundance=NULL, trace=0)
 	}
 
 	par0 <- c(log(0.1),0)
-	out <- optim(par0,fun,y=y,design=design,offset=offset,abundance=abundance,control=list(trace=trace))
+	out <- optim(par0,fun,y=y,design=design,offset=offset,abundance=abundance,control=list(trace=trace),method=method.optim)
 	out$dispersion <- exp(out$par[1]+out$par[2]*abundance)
 	out$abundance <- abundance
 	out
