1f4c02446af20e0ab595801342ebc630ca4ee716
smyth@wehi.edu.au <smyth@wehi.edu.au@bc3139a8-67e5-0310-9ffc-ced21a209358>
2011-05-07 06:23:06 +0000

    New function cutWithMinN.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/edgeR@55315 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/cutWithMinN.R
@@ -0,0 +1,36 @@
+cutWithMinN <- function(x, intervals=2, min.n=1)
+#	Cut numeric x into intervals, as equally spaced as possible subject
+#	to including a minimum number of values in each interval
+#	Gordon Smyth
+#	7 May 2011
+{
+#	Check input
+	if(any(is.na(x))) stop("missing values not allowed")
+	x <- as.numeric(x)
+	intervals <- as.integer(intervals)
+	min.n <- as.integer(min.n)
+	if(length(x) < intervals*min.n) stop("length(x) < intervals*min.n")
+
+#	First try
+	z <- cut(x,breaks=intervals,labels=FALSE)
+	n <- tabulate(z)
+	if(all(n>=min.n)) return(z)
+
+	if(length(unique(x)) < intervals*min.n) stop("too few unique values for x")
+
+	breaks.eqx <- seq(from=min(x),to=max(x),length.out=intervals+1L)
+	breaks.eqx[1] <- breaks.eqx[1]-1
+	breaks.eqx[intervals+1L] <- breaks.eqx[intervals+1L]+1
+	breaks.eqn <- quantile(x,p=seq(from=0,to=1,length.out=intervals+1L))
+	breaks.eqn[1] <- breaks.eqn[1]-1
+	breaks.eqn[intervals+1L] <- breaks.eqn[intervals+1L]+1
+
+	for (i in 1:10) {
+		breaks <- (i*breaks.eqn+(10-i)*breaks.eqx)/10
+		z <- cut(x,breaks=breaks,labels=FALSE)
+		n <- tabulate(z)
+		if(all(n>=min.n)) return(z)
+	}
+
+	stop("function has failed, perhaps because of too many tied values?")
+}
