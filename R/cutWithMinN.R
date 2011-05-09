4b80526acbb91af791af0188bad74261e4314b9b
smyth@wehi.edu.au <smyth@wehi.edu.au@bc3139a8-67e5-0310-9ffc-ced21a209358>
2011-05-09 06:31:05 +0000

    Documentation and code fixes for cutWithMinN.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/edgeR@55347 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/cutWithMinN.R
+++ R/cutWithMinN.R
@@ -5,16 +5,18 @@ cutWithMinN <- function(x, intervals=2, min.n=1)
 #	7 May 2011.  Last modified 9 May 2011.
 {
 #	Check input
-	if(any(is.na(x))) stop("missing values not allowed")
 	x <- as.numeric(x)
+	isna <- is.na(x)
+	if(any(isna)) {
+		group <- rep.int(NA,length(x))
+		out <- Recall(x=x[!isna],intervals=intervals,min.n=min.n)
+		group[!isna] <- out$group
+		out$group <- group
+		return(out)
+	}
 	intervals <- as.integer(intervals)
 	min.n <- as.integer(min.n)
-	if(length(x) < intervals*min.n) stop("length(x) < intervals*min.n")
-
-#	First try
-	z <- cut(x,breaks=intervals,labels=FALSE)
-	n <- tabulate(z)
-	if(all(n>=min.n)) return(list(group=z,breaks=breaks))
+	if(length(x) < intervals*min.n) stop("two few observations: length(x) < intervals*min.n")
 
 	if(length(unique(x)) < intervals*min.n) stop("too few unique values for x")
 
@@ -24,6 +26,11 @@ cutWithMinN <- function(x, intervals=2, min.n=1)
 	breaks.eqn <- quantile(x,p=seq(from=0,to=1,length.out=intervals+1L))
 	breaks.eqn[1] <- breaks.eqn[1]-1
 	breaks.eqn[intervals+1L] <- breaks.eqn[intervals+1L]+1
+
+#	First try
+	z <- cut(x,breaks=intervals,labels=FALSE)
+	n <- tabulate(z)
+	if(all(n>=min.n)) return(list(group=z,breaks=breaks.eqx))
 
 	for (i in 1:10) {
 		breaks <- (i*breaks.eqn+(10-i)*breaks.eqx)/10
