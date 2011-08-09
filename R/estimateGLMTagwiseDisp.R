7a71a418143d9a81cb041c1877ae00e7dc0101a9
y.chen <y.chen@bc3139a8-67e5-0310-9ffc-ced21a209358>
2011-08-09 07:02:57 +0000

    Make change to estimateGLMTagwiseDisp.R and dispCoxReidInterpolateTagwise.R so that the APLs are shrunk towards the common APL when common dispersions estimates are used.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/edgeR@57318 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/estimateGLMTagwiseDisp.R
+++ R/estimateGLMTagwiseDisp.R
@@ -3,33 +3,32 @@ UseMethod("estimateGLMTagwiseDisp")
 
 estimateGLMTagwiseDisp.DGEList <- function(y, design, offset=NULL, method="trend", ...)
 {
-    if( is.null(offset) )
-        offset <- getOffset(y)
-    method <- match.arg(method, c("trend","common"))
-    if(method=="trend") {
-        if( is.null(y$trended.dispersion) & is.null(y$common.dispersion) ) stop("method is trend, but DGEList object has a NULL trended.dispersion slot. Run estimateGLMTrendedDisp on DGEList object before estimateGLMTagwiseDisp to smooth tagwise dispersions towards trended dispersions.\n")
-        if( is.null(y$trended.dispersion) & !is.null(y$common.dispersion) ) {
-            warning("method is trend, but DGEList object has a NULL trended.dispersion slot. Common dispersion value used for this calculation.\n")
-            dispersion <- y$common.dispersion
-        } else {
-            dispersion <- y$trended.dispersion
-            abundance <- y$abundance
-        }
-    }
-    if(method=="common") {
-        if( is.null(y$common.dispersion) ) stop("method is common, but DGEList object has a NULL common.dispersion slot. Run estimateGLMCommonDisp on DGEList object before estimateGLMTagwiseDisp to smooth tagwise dispersions towards a common value.\n")
-        dispersion <- rep(y$common.dispersion, nrow(y))
-        abundance <- mglmOneGroup(y$counts,offset=offset)
-    }
-	d <- estimateGLMTagwiseDisp(y=y$counts, design=design, offset=offset, dispersion=dispersion, abundance=abundance, ...)
+	if( is.null(offset) )
+		offset <- getOffset(y)
+	method <- match.arg(method, c("trend","common"))
+	if(method=="trend") {
+		if( is.null(y$trended.dispersion) & is.null(y$common.dispersion) ) stop("method is trend, but DGEList object has a NULL trended.dispersion slot. Run estimateGLMTrendedDisp on DGEList object before estimateGLMTagwiseDisp to smooth tagwise dispersions towards trended dispersions.\n")
+		if( is.null(y$trended.dispersion) & !is.null(y$common.dispersion) ) {
+			warning("method is trend, but DGEList object has a NULL trended.dispersion slot. Common dispersion value used for this calculation.\n")
+			dispersion <- y$common.dispersion
+		} else {
+			dispersion <- y$trended.dispersion
+			abundance <- y$abundance
+		}
+	}
+	if(method=="common") {
+		if( is.null(y$common.dispersion) ) stop("method is common, but DGEList object has a NULL common.dispersion slot. Run estimateGLMCommonDisp on DGEList object before estimateGLMTagwiseDisp to smooth tagwise dispersions towards a common value.\n")
+		dispersion <- rep(y$common.dispersion, nrow(y))
+		abundance <- mglmOneGroup(y$counts,offset=offset)
+	}
+	d <- estimateGLMTagwiseDisp(y=y$counts, design=design, offset=offset, dispersion=dispersion, abundance=abundance, method=method, ...)
 	y$tagwise.dispersion <- d
-    y$abundance <- abundance
+	y$abundance <- abundance
 	y
 }
 
-estimateGLMTagwiseDisp.default <- function(y, design, offset=NULL, dispersion, ...)
+estimateGLMTagwiseDisp.default <- function(y, design, offset=NULL, dispersion, method, ...)
 {
 	y <- as.matrix(y)
-    dispCoxReidInterpolateTagwise(y, design, offset=offset, dispersion, ...)
+	dispCoxReidInterpolateTagwise(y, design, offset=offset, dispersion, method=method, ...)
 }
-
