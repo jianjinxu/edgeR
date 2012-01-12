714b65015df9b5be45ebdfd44300b3fa087bfb05
smyth@wehi.edu.au <smyth@wehi.edu.au@bc3139a8-67e5-0310-9ffc-ced21a209358>
2012-01-12 05:16:43 +0000

    New function predFC for damping down fold changes.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/edgeR@62010 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/predFC.R
@@ -0,0 +1,41 @@
+predFC <- function(y,design,prior.total.count=1,offset=NULL,dispersion=NULL) 
+UseMethod("predFC")
+
+predFC.DGEList <- function(y,design,prior.total.count=1,offset=NULL,dispersion=NULL)
+{
+	if(is.null(offset)) offset <- getOffset(y)
+	if(is.null(dispersion)) dispersion <- getDispersion(y)$dispersion
+	if(is.null(dispersion)) {
+		dispersion <- 0
+		message("dispersion set to zero")
+	}
+	predFC(y=y$counts,design=design,prior.total.count=prior.total.count,offset=offset,dispersion=dispersion)
+}
+
+predFC.default <- function(y,design,prior.total.count=1,offset=log(colSums(y)),dispersion=0)
+#	Shrink glm estimates by augmenting data counts towards a constant
+#	17 Aug 2011
+{
+	y <- as.matrix(y)
+	if(missing(design)) stop("design must be set")
+	ngenes <- nrow(y)
+	nsamples <- ncol(y)
+	pfc <- rep(0,ngenes)
+	names(pfc) <- rownames(y)
+	offset <- expandAsMatrix(offset,dim(y))
+#	dispersion <- rep(dispersion,ngenes)
+
+#	Add one to rowsum, in proportion to library sizes
+	lib.size <- exp(offset)
+	total.lib.size <- rowSums(lib.size)
+	proportion <- lib.size/total.lib.size
+	y.augmented <- y+proportion*prior.total.count
+
+#	Adjust offsets to keep overall mean similar
+#	total.count <- rowSums(y)
+#	offset.augmented <- offset+log((total.count+1)/pmax(total.count,0.5))
+
+   g <- glmFit(y.augmented,design,offset=offset,dispersion=dispersion)
+   log2(exp(g$coef))
+}
+
