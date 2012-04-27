86bfd73f8dafa6087691def7ec79e54f75f89cfd
smyth@wehi.edu.au <smyth@wehi.edu.au@bc3139a8-67e5-0310-9ffc-ced21a209358>
2012-04-27 14:11:50 +0000

    Bug fix to plotBCV.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/edgeR@65480 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/plotBCV.R
+++ R/plotBCV.R
@@ -1,23 +1,19 @@
-plotBCV <- function( object , xlab="Abundance (log2 counts per million)", ylab="Biological coefficient of variation (BCV)", ...) 
-## Make a plot of biological coefficient of variation against abundance (counts per million)
-## Davis McCarthy and Yunshun Chen
-## Created 18 January 2012. Last modified 26 April 2012.
+plotBCV <- function(object, xlab="logCPM", ylab="Biological coefficient of variation", pch=16, cex=0.2, ...)
+#	Plot of biological coefficient of variation against abundance (log CPM)
+#	Davis McCarthy, Yunshun Chen, Gordon Smyth.
+#	Created 18 January 2012.  Last modified 27 April 2012.
 {
-	if( !is(object, "DGEList") )
-		stop("First argument must be a DGEList object.")
-	if( is.null( object$tagwise.dispersion ) )
-		stop("Tagwise dispersion estimates not found: object$tagwise.dispersion is NULL. Run either estimateTagwiseDisp() or estimateGLMTrendedDisp() then estimateGLMTagwiseDisp() before trying to plot BCV estimates.")
-	if( is.null( object$abundance ) )
-		object$abundance <- mglmOneGroup(as.matrix(object), dispersion=object$tagwise.dispersion)
-	abund.cpm <- log2(exp(object$abundance)) + log2(1e6)
-	plot(abund.cpm, sqrt(object$tagwise.dispersion), ylab=ylab, xlab=xlab, ...)
-	if( !is.null(object$trended.dispersion) ) {
-		o <- order(abund.cpm)
-		lines(abund.cpm[o], sqrt(object$trended.dispersion)[o], col="blue", lwd=2)
+	if(!is(object,"DGEList")) stop("object must be a DGEList object.")
+	if(is.null(object$tagwise.dispersion)) stop("Tagwise dispersion estimates not found. Run either estimateTagwiseDisp() or estimateGLMTrendedDisp() then estimateGLMTagwiseDisp() before trying to plot BCV estimates.")
+	if(is.null(object$abundance)) object$abundance <- mglmOneGroup(object$counts, offset=getOffset(object), dispersion=object$tagwise.dispersion)
+	log2CPM <- object$abundance/log(2) + log2(1e6)
+	plot(log2CPM, sqrt(object$tagwise.dispersion), ylab=ylab, xlab=xlab, pch=pch, cex=cex, ...)
+	if(!is.null(object$trended.dispersion)) {
+		o <- order(log2CPM)
+		lines(log2CPM[o], sqrt(object$trended.dispersion)[o], col="blue", lwd=2)
 	}
-	if( !is.null(object$common.dispersion) ) {
-		abline(h=sqrt(object$common.dispersion), col="red", lwd=2)
-	}
+	if(!is.null(object$common.dispersion)) abline(h=sqrt(object$common.dispersion), col="red", lwd=2)
+	legend("topright",legend=c("Trend","Common"),lwd=2,col=c("blue","red"))
 	invisible()
 }
 
