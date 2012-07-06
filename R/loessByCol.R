4aca6a887f4ab41b3f46dda78a50901d46e6892f
smyth@wehi.edu.au <smyth@wehi.edu.au@bc3139a8-67e5-0310-9ffc-ced21a209358>
2012-07-06 07:40:24 +0000

    Cosmetic edits to loessByCol.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/edgeR@67327 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/loessByCol.R
+++ R/loessByCol.R
@@ -1,11 +1,11 @@
 loessByCol <- function(y, x=NULL, span=0.5)
 # Calls a C++ function to do the dirty work of fitting a degree-0,
-# non-robustified LOWESS curve through each column of a matrix
-# of data points.
-# C++ version by Aaron Lun, 26 June 2012.
+# non-robustified loess curve through each column of a matrix.
+
+# C++ version by Aaron Lun, 26 June 2012.  Last modified 6 July 2012.
 # Replaces:
 # Rcode version by Davis McCarthy, May 2010.
-# loess version by Yunshun Chen, 08 May 2012.
+# simpleLoess version by Yunshun Chen, 08 May 2012.
 {
     y <- as.matrix(y)
     ntags <- nrow(y)
@@ -16,10 +16,11 @@ loessByCol <- function(y, x=NULL, span=0.5)
     y<-y[x.order,,drop=FALSE]
     x<-x[x.order]
 
-    nspan <- floor(span*ntags)
-    nspan <- min(ntags,nspan)
+    nspan <- min(floor(span*ntags), ntags)
     if(nspan<=1) {
-    	return(list(fitted.values=y,leverages=rep(1,ntags)))
+       fitted <- list(fitted.values=y,leverages=rep(1,ntags))
+       names(fitted$leverages) <- rownames(y)
+       return(fitted)
     }
 
     # Passing to the compiled code.
@@ -30,7 +31,9 @@ loessByCol <- function(y, x=NULL, span=0.5)
     fitted[[2]][x.order]<-fitted[[2]]
 
     # Beautifying.
-    names(fitted)<-c("fitted.values", "leverages")
+    names(fitted) <- c("fitted.values", "leverages")
+    dimnames(fitted$fitted.values) <- dimnames(y)
+    names(fitted$leverages) <- rownames(y)
 
-    return(fitted)
+    fitted
 }
