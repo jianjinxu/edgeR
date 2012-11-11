78db6f189711a54a2892a1bafca4199c74895362
smyth@wehi.edu.au <smyth@wehi.edu.au@bc3139a8-67e5-0310-9ffc-ced21a209358>
2012-11-11 05:52:17 +0000

    Fix to plotMDS.DGEList.R to use norm.factors consistently.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/edgeR@71119 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/plotMDS.DGEList.R
+++ R/plotMDS.DGEList.R
@@ -1,7 +1,7 @@
 plotMDS.DGEList <- function (x, top=500, labels=colnames(x), col=NULL, cex=1, dim.plot=c(1, 2), ndim=max(dim.plot), xlab=paste("Dimension",dim.plot[1]), ylab=paste("Dimension",dim.plot[2]), ...)
 #	Multidimensional scaling plot of digital gene expression profiles
 #	Yunshun Chen, Mark Robinson and Gordon Smyth
-#	23 May 2011.  Last modified 23 July 2012.
+#	23 May 2011.  Last modified 11 Nov 2012.
 {
 #	Remove rows with missing or Inf values
 	ok <- is.finite(x$counts)
@@ -30,15 +30,14 @@ plotMDS.DGEList <- function (x, top=500, labels=colnames(x), col=NULL, cex=1, di
 		subdata<-x$counts
 	}
 
-#	gm <- function(x) exp(mean(log(x)))
+	lib.size <- x$samples$lib.size * x$samples$norm.factors
 	myFun <- function(delta, y, ...) sum(condLogLikDerDelta(y, delta, ...))
 
 	for (i in 2:(nsamples)) {
 		for (j in 1:(i - 1))  {
 			mm <- subdata[,c(i,j)]
 			rs5 <- rowSums(mm) > 5
-#			norm <- t(t(mm)/colSums(mm)) * gm(colSums(mm))
-			lib<-x$samples$lib.size[c(i, j)]
+			lib <- lib.size[c(i, j)]
 			norm <- t(t(mm)/lib) * exp(mean(log(lib)))
 			delta <- optimize(myFun, interval = c(0.0001,.99), tol = 0.000001, maximum = TRUE, y = norm[rs5,], der = 0)
 			dd[i, j] = sqrt( delta$maximum / (1-delta$maximum) )
@@ -47,7 +46,7 @@ plotMDS.DGEList <- function (x, top=500, labels=colnames(x), col=NULL, cex=1, di
 
 #	Securing against negative eigenvalues with non-Euclidian distance matrices.
 	a1 <- cmdscale(as.dist(dd), k = ndim)
-	ndiff<-ndim-ncol(a1)
+	ndiff <- ndim-ncol(a1)
 	if (ndiff > 0) a1<-cbind(a1, matrix(runif(ndiff*nsamples, -1e-6, 1e-6), ncol=ndiff, nrow=nsamples))
 
 	mds <- new("MDS",list(dim.plot=dim.plot,distance.matrix=dd,cmdscale.out=a1,top=top))
