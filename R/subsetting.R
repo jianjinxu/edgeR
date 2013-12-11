8fa4269598f569baf9951f0ab1e3bacb96af64a2
smyth@wehi.edu.au <smyth@wehi.edu.au@bc3139a8-67e5-0310-9ffc-ced21a209358>
2013-12-11 05:55:12 +0000

    3.5.22. Simplification of the subsetting code for DGEList, DGEExact, DGEGLM, DGELRT and TopTags data objects using the new utility function subsetListOfArrays in the limma package.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/edgeR@84162 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/subsetting.R
+++ R/subsetting.R
@@ -1,93 +1,44 @@
 #  SUBSET DATA SETS
 
 assign("[.DGEList",
-function(object, i, j, ...) {
+function(object, i, j)
 #  Subsetting for DGEList objects
-#  Davis McCarthy, Gordon Smyth 
-#  24 September 2009.  Last modified 7 Dec 2013.
+#  24 September 2009.  Last modified 11 Dec 2013.
+{  
+#	Recognized components
+	IJ <- c("counts","pseudo.counts","offset","weights")
+	IX <- c("genes")
+	JX <- c("samples")
+	I  <- c("AveLogCPM","trended.dispersion","tagwise.dispersion","prior.n","prior.df")
+#	Obsolete <- c("conc","infos","all.zeros")
 
-	if(nargs() != 3) stop("Two subscripts required",call.=FALSE)
-	if(missing(i))
-		if(missing(j))
-			return(object)
-		else {
-			object$counts <- object$counts[,j,drop=FALSE]
-			object$samples <- droplevels(object$samples[j,,drop=FALSE])
-			object$pseudo.counts <- object$pseudo.counts[,j,drop=FALSE]
-			object$offset <- object$offset[,j,drop=FALSE]
-		}
-	else {
-		if(is.character(i)) {
-			i <- match(i,rownames(object$counts))
-			i <- i[!is.na(i)]
-		}
-		# alter regardless of 'j'
-		object$conc$conc.common <- object$conc$conc.common[i]
-		object$conc$conc.group <- object$conc$conc.group[i,,drop=FALSE]
-		object$trended.dispersion <- object$trended.dispersion[i]
-		object$tagwise.dispersion <- object$tagwise.dispersion[i]
-		object$infos <- object$infos[i]
-		object$genes <- object$genes[i,,drop=FALSE]
-		object$all.zeros <- object$all.zeros[i]
-		object$offset <- object$offset[i,,drop=FALSE]
-		object$AveLogCPM <- object$AveLogCPM[i]
-
-		# alter depending on 'j'
-		if(missing(j)) {
-			object$counts <- object$counts[i,,drop=FALSE]
-			object$abundance <- object$abundance[i]
-			object$pseudo.counts <- object$pseudo.counts[i,,drop=FALSE]
-			object$weights <- object$weights[i,,drop=FALSE]
-		} else {
-			object$counts <- object$counts[i,j,drop=FALSE]
-			object$samples <- droplevels(object$samples[j])
-			object$pseudo.counts <- object$pseudo.counts[i,j,drop=FALSE]
-			object$weights <- object$weights[i,j,drop=FALSE]
-		}
-	}
-	object
+	subsetListOfArrays(object,i,j,IJ=IJ,IX=IX,I=I,JX=JX)
 })
 
-
 assign("[.DGEGLM",
-function(object, i, j, ...)
+function(object, i, j)
 #  Subsetting for DGEGLM objects
-#  Davis McCarthy, Gordon Smyth
-#  11 May 2011.  Last modified 8 April 2013.
+#  11 May 2011.  Last modified 11 Dec 2013.
 {
-	if(nargs() != 3) stop("Two subscripts required",call.=FALSE)
-	if(!missing(j))
-		stop("Subsetting columns not allowed for DGEGLM object. Try subsetting elements of DGEGLM object instead.",call.=FALSE)
-	if(!missing(i)) {
-		object$coefficients <- object$coefficients[i,,drop=FALSE]
-		object$df.residual <- object$df.residual[i]
-		object$deviance <- object$deviance[i]
-		object$offset <- object$offset[i,,drop=FALSE]
-		object$genes <- object$genes[i,,drop=FALSE]
-		object$trended.dispersion <- object$trended.dispersion[i]
-		object$tagwise.dispersion <- object$tagwise.dispersion[i]
-		if(length(object$dispersion)>1) object$dispersion <- object$dispersion[i]
-		object$weights <- object$weights[i,,drop=FALSE]
-		object$fitted.values <- object$fitted.values[i,,drop=FALSE]
-		object$abundance <- object$abundance[i]
-		object$AveLogCPM <- object$AveLogCPM[i]
-		object$counts <- object$counts[i,,drop=FALSE]
-		object$iter <- object$iter[i]
-		object$failed <- object$failed[i]
-	}
-	object
+	if(!missing(j)) stop("Subsetting columns not allowed for DGEGLM object.",call.=FALSE)
+
+#	Recognized components
+	IJ <- character(0)
+	IX <- c("counts","offset","weights","genes","coefficients","fitted.values")
+	I  <- c("AveLogCPM","trended.dispersion","tagwise.dispersion","prior.n","prior.df","dispersion","df.residual","deviance","iter","failed")
+	JX <- c("samples","design")
+
+	subsetListOfArrays(object,i,j,IJ=IJ,IX=IX,I=I,JX=JX)
 })
 
 
 assign("[.DGEExact",
-function(object, i, j, ...)
+function(object, i, j)
 #  Subsetting for DGEExact objects
 #  Davis McCarthy, Gordon Smyth
-#  6 October 2010.  Last modified 8 April 2013.
+#  6 October 2010.  Last modified 11 Dec 2013.
 {
-	if(nargs() != 3) stop("Two subscripts required",call.=FALSE)
-	if(!missing(j))
-		stop("Subsetting columns not allowed for DGEExact object. Try subsetting object$table instead.",call.=FALSE)
+	if(!missing(j)) stop("Subsetting columns not allowed for DGEExact objects.",call.=FALSE)
 	if(!missing(i)) {
 		object$table <- object$table[i,,drop=FALSE]
 		object$genes <- object$genes[i,,drop=FALSE]
@@ -97,59 +48,27 @@ function(object, i, j, ...)
 
 
 assign("[.DGELRT",
-function(object, i, j, ...)
+function(object, i, j)
 #  Subsetting for DGELRT objects
-#  Davis McCarthy, Gordon Smyth	
-#  6 April 2011.  Last modified 8 April 2013.
+#  6 April 2011.  Last modified 11 Dec 2013.
 {
-	if(nargs() != 3) stop("Two subscripts required",call.=FALSE)
-	if(!missing(j))
-		stop("Subsetting columns not allowed for DGELRT object. Try subsetting object$table instead.",call.=FALSE)
-	if(!missing(i)) {
-		object$table <- object$table[i,,drop=FALSE]
-		object$genes <- object$genes[i,,drop=FALSE]
-		object$abundance <- object$abundance[i]
-		object$trended.dispersion <- object$trended.dispersion[i]
-		object$tagwise.dispersion <- object$tagwise.dispersion[i]
-		object$dispersion <- object$dispersion[i]
-		object$coefficients <- object$coefficients[i,,drop=FALSE]
-		object$AveLogCPM <- object$AveLogCPM[i]
-		object$counts <- object$counts[i,,drop=FALSE]
-		object$iter <- object$iter[i]
-		object$failed <- object$failed[i]
-		object$df.test <- object$df.test[i]
-		object$offset <- object$offset[i,,drop=FALSE]
-		object$fitted.values <- object$fitted.values[i,,drop=FALSE]
-		object$deviance <- object$deviance[i]
-		object$df.residual <- object$df.residual[i]
-	}
-	object
+	if(!missing(j)) stop("Subsetting columns not allowed for DGELRT object.",call.=FALSE)
+
+#	Recognized components
+	IJ <- character(0)
+	IX <- c("counts","offset","weights","genes","coefficients","fitted.values","table")
+	I  <- c("AveLogCPM","trended.dispersion","tagwise.dispersion","prior.n","prior.df","dispersion","df.residual","deviance","iter","failed","df.test")
+	JX <- character(0)
+
+	subsetListOfArrays(object,i,j,IJ=IJ,IX=IX,I=I,JX=JX)
 })
 
 
 assign("[.TopTags",
-function(object, i, j, ...)
+function(object, i, j)
 #  Subsetting for TopTags objects
-#  Gordon Smyth 
-#  7 October 2009. Last modified 8 April 2013.
+#  7 October 2009. Last modified 11 Dec 2013.
 {
-	if(nargs() != 3) stop("Two subscripts required",call.=FALSE)
-	if(missing(i))
-		if(missing(j))
-			return(object)
-		else {
-			object$table <- object$table[,j,drop=FALSE]
-		}
-	else {
-		if(is.character(i)) {
-			i <- match(i,rownames(object$counts))
-			i <- i[!is.na(i)]
-		}
-		if(missing(j)) {
-			object$table <- object$table[i,,drop=FALSE]
-		} else {
-			object$table <- object$table[i,j,drop=FALSE]
-		}
-	}
+	if(!missing(i) || !missing(j)) object$table <- object$table[i,j,drop=FALSE]
 	object
 })
