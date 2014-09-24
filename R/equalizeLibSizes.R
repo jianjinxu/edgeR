5c02bd0ce16496106913a7382525bf25966353ee
smyth@wehi.edu.au <smyth@wehi.edu.au@bc3139a8-67e5-0310-9ffc-ced21a209358>
2014-09-24 02:57:28 +0000

    equalizeLibSizes() now gets dispersion from object if it exists.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/edgeR@94467 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/equalizeLibSizes.R
+++ R/equalizeLibSizes.R
@@ -1,8 +1,8 @@
-equalizeLibSizes <- function(object, dispersion=0, common.lib.size=NULL)
+equalizeLibSizes <- function(object, dispersion=NULL, common.lib.size=NULL)
 #	Normalize counts so that the library sizes can be treated as equal.
 #	Uses a quantile-to-quantile transformation so that new count counts are equivalent deviates on the equalized scale.
 #	Davis McCarthy, Gordon Smyth.
-#	Created July 2009. Last modified 25 July 2012.
+#	Created July 2009. Last modified 24 September 2014.
 {
 	d <- dim(object)
 	ntags <- d[1]
@@ -10,7 +10,9 @@ equalizeLibSizes <- function(object, dispersion=0, common.lib.size=NULL)
 	lib.size <- object$samples$lib.size * object$samples$norm.factors
 	if(is.null(common.lib.size)) common.lib.size <- exp(mean(log(lib.size)))
 	levs.group <- unique(object$samples$group)
-	if(length(dispersion)==1) dispersion <- rep(dispersion,ntags)
+
+	if(is.null(dispersion)) dispersion <- getDispersion(object)
+	if(is.null(dispersion)) dispersion <- 0.05
 
 	input.mean <- output.mean <- matrix(0,ntags,nlibs)
 	for(i in 1:length(levs.group)) {
@@ -18,7 +20,7 @@ equalizeLibSizes <- function(object, dispersion=0, common.lib.size=NULL)
 		beta <- mglmOneGroup(object$counts[,j,drop=FALSE],dispersion=dispersion,offset=log(lib.size[j]))
 		lambda <- exp(beta)
 		input.mean[,j] <- matrix(lambda,ncol=1) %*% matrix(lib.size[j],nrow=1)
-		output.mean[,j] <- matrix(lambda, ncol=1) %*% matrix(common.lib.size, nrow=1, ncol=sum(j));
+		output.mean[,j] <- matrix(lambda, ncol=1) %*% matrix(common.lib.size, nrow=1, ncol=sum(j))
 	}
 	pseudo <- q2qnbinom(object$counts, input.mean=input.mean, output.mean=output.mean, dispersion=dispersion)
 	pseudo[pseudo<0] <- 0
