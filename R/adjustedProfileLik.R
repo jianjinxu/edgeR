4f5a4c6837accc8e4f706cb9e526aad7d8dd336d
y.chen <y.chen@bc3139a8-67e5-0310-9ffc-ced21a209358>
2014-10-03 05:03:18 +0000

    Changed adjustedProfileLik to accept and return GLM coefficients.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/edgeR@94898 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/adjustedProfileLik.R
+++ R/adjustedProfileLik.R
@@ -1,10 +1,10 @@
-adjustedProfileLik <- function(dispersion, y, design, offset, weights=NULL, adjust=TRUE)
+adjustedProfileLik <- function(dispersion, y, design, offset, weights=NULL, adjust=TRUE, start=NULL, get.coef=FALSE)
 # tagwise Cox-Reid adjusted profile likelihoods for the dispersion
 # dispersion can be scalar or tagwise vector
 # y is matrix: rows are genes/tags/transcripts, columns are samples/libraries
 # offset is matrix of the same dimensions as y
 # Yunshun Chen, Gordon Smyth, Aaron Lun
-# Created June 2010. Last modified 17 Feb 2014.
+# Created June 2010. Last modified 11 Sep 2014.
 {
 	if(any(dim(y)!=dim(offset))) offset <- expandAsMatrix(offset,dim(y))
 	ntags <- nrow(y)
@@ -13,7 +13,7 @@ adjustedProfileLik <- function(dispersion, y, design, offset, weights=NULL, adju
        
 #	Fit tagwise linear models. This is actually the most time-consuming
 #	operation that I can see for this function.
-	fit <- glmFit(y,design=design,dispersion=dispersion,offset=offset,prior.count=0,weights=weights)
+	fit <- glmFit(y,design=design,dispersion=dispersion,offset=offset,prior.count=0,weights=weights,start=start)
 
 #	Compute log-likelihood.
 	mu <- fit$fitted
@@ -49,7 +49,12 @@ adjustedProfileLik <- function(dispersion, y, design, offset, weights=NULL, adju
 		cr <- .Call(.cR_cr_adjust, t(W), design, nrow(design))
 		if (is.character(cr)) { stop(cr) }
 	}
- 
-	return(loglik - cr)
+
+#	Deciding what to return.
+    if (get.coef) { 
+		return(list(apl=loglik-cr, beta=fit$coefficients))
+	} else {
+		return(loglik - cr)
+	}
 }
 
