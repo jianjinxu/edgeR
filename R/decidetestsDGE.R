d98915e7bd9c691fe35a4e2f2c2163d566bbc53b
m.robinson <m.robinson@bc3139a8-67e5-0310-9ffc-ced21a209358>
2014-10-17 11:40:35 +0000

    added lfc (log-fold-change) argument to decideTestsDGE + docs
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/edgeR@95679 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/decidetestsDGE.R
+++ R/decidetestsDGE.R
@@ -1,12 +1,13 @@
 #  DECIDETESTSDGE.R
 
-decideTestsDGE <- function(object,adjust.method="BH",p.value=0.05)
+decideTestsDGE <- function(object,adjust.method="BH",p.value=0.05,lfc=0)
     ##	Accept or reject hypothesis tests across genes and contrasts
     ##	Davis McCarthy
-    ##	15 August 2010. Last modified 19 Jan 2012.
+    ##	15 August 2010. Last modified 17 Oct 2014 (Mark Robinson).
 {
     if(!is(object,"DGEExact") & !is(object,"DGELRT")) stop("Need DGEExact or DGELRT object") # Expects a DGEExact or DGELRT object
-    decideTests(new("MArrayLM", list(p.value=object$table$PValue, coefficients=object$table$logFC)), method="separate", adjust.method=adjust.method, p.value=p.value)
+    decideTests(new("MArrayLM", list(p.value=object$table$PValue, coefficients=object$table$logFC)), 
+                    method="separate", adjust.method=adjust.method, p.value=p.value, lfc=lfc)
 }
 
 
