5d77bba59196ff8436b73a7fd31b9d3798d8a37b
a.lun <a.lun@bc3139a8-67e5-0310-9ffc-ced21a209358>
2015-03-09 04:05:12 +0000

    Merged c100388 from devel.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/branches/RELEASE_3_0/madman/Rpacks/edgeR@100389 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zscoreNBinom.R
+++ R/zscoreNBinom.R
@@ -2,22 +2,32 @@
 
 zscoreNBinom <- function(q, size, mu) 
 #  Z-score equivalents for negative binomial deviates
-#  Gordon Smyth
-#  10 December 2011
+#  Gordon Smyth, Aaron Lun
+#  created 10 December 2011
+#  last modified 9 March 2015
 {
 	z <- round(q)
 	n <- length(q)
 	size <- rep(size,length.out=n)
 	mu <- rep(mu,length.out=n)
-	d <- dnbinom(q,size=size,mu=mu)
+	d <- dnbinom(q,size=size,mu=mu, log=TRUE)
 	up <- (q >= mu)
+
 	if(any(up)) {
-		p <- pnbinom(q[up],size=size[up],mu=mu[up],lower.tail=FALSE)
-		z[up] <- qnorm(p+d[up]/2,lower.tail=FALSE)
+		p <- pnbinom(q[up],size=size[up],mu=mu[up],lower.tail=FALSE, log.p=TRUE)
+		refined.p <- p + suppressWarnings(log1p(exp(d[up] - p)/2))
+		z[up] <- suppressWarnings(qnorm(refined.p, lower.tail=FALSE, log.p=TRUE))
 	}
 	if(any(!up)) {
-		p <- pnbinom(q[!up],size=size[!up],mu=mu[!up],lower.tail=TRUE)
-		z[!up] <- qnorm(p-d[!up]/2,lower.tail=TRUE)
+		p <- pnbinom(q[!up],size=size[!up],mu=mu[!up],lower.tail=TRUE, log.p=TRUE)
+		refined.p <- p + suppressWarnings(log1p(-exp(d[!up] - p)/2))
+		z[!up] <- suppressWarnings(qnorm(refined.p,lower.tail=TRUE, log.p=TRUE))
+	}
+
+	not.fin <- !is.finite(z)
+	if (any(not.fin)) { 
+		z[not.fin] <- (2L*up[not.fin] -1L) * sqrt(nbinomUnitDeviance(q[not.fin], 
+			mean=mu[not.fin], dispersion=1/size[not.fin])) 
 	}
 	z
 }
