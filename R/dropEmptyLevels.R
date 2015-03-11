34e6dfb7d730bb238262ec3863b88bbdb7cafb9b
y.chen <y.chen@bc3139a8-67e5-0310-9ffc-ced21a209358>
2015-03-11 23:58:14 +0000

    edgeR 3.9.11 - Fix dropEmptyLevels().
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/edgeR@100570 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/dropEmptyLevels.R
+++ R/dropEmptyLevels.R
@@ -1,18 +1,12 @@
 dropEmptyLevels <- function(x)
 #	Drop levels of a factor that don't occur
 #	Gordon Smyth
-#	Created 25 March 2012.  Last modified 8 Feb 2015.
+#	Created 25 March 2012.  Last modified 6 March 2015.
 {
 	if(is.factor(x)) {
-		v <- as.integer(x)
-		i <- which(tabulate(v)>0L)
-		if(length(i) < nlevels(x)) {
-			levels(v) <- levels(x)[i]
-			class(v) <- class(x)
-			return(v)
-		} else {
-			return(x)
-		}
+		i <- which(tabulate(as.integer(x))>0L)
+		if(length(i) < nlevels(x)) x <- factor(x, levels=levels(x)[i])
+		return(x)
 	} else {
 		return(factor(x))
 	}
