f4ca9d70f0323d65d925287e72116d6f39ec11a6
y.chen <y.chen@bc3139a8-67e5-0310-9ffc-ced21a209358>
2015-09-24 04:57:53 +0000

    Edits to expandAsMatrix() to allow users to specify whether the matrix is filled by columns or by rows if the length of the input vector is equal to both dimensions.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/branches/RELEASE_3_1/madman/Rpacks/edgeR@108789 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/expandAsMatrix.R
+++ R/expandAsMatrix.R
@@ -1,20 +1,21 @@
 
-expandAsMatrix <- function(x,dim=NULL)
+expandAsMatrix <- function(x,dim=NULL,byrow=TRUE)
 #	Convert scalar, row or column vector, or matrix, to be a matrix
 #	Gordon Smyth
-#	26 Jan 2011.  Last modified 26 Jan 2011.
+#	26 Jan 2011.  Last modified 27 July 2015 (Yunshun Chen).
 {
 #	Check dim argument
 	if(is.null(dim)) return(as.matrix(x))
 	if(length(dim)<2) stop("dim must be numeric vector of length 2")
 	dim <- round(dim[1:2])
-	if(any(dim<1)) stop("zero or negative dimensions not allowed")
+	if(any(dim<0)) stop("negative dimensions not allowed")
 
 #	x is a vector
 	dx <- dim(x)
 	if(is.null(dx)) {
 		lx <- length(x)
 		if(lx==1) return(matrix(x,dim[1],dim[2]))
+		if(lx==dim[1] & lx==dim[2]) return(matrix(x,dim[1],dim[2],byrow=byrow))
 		if(lx==dim[2]) return(matrix(x,dim[1],dim[2],byrow=TRUE))
 		if(lx==dim[1]) return(matrix(x,dim[1],dim[2],byrow=FALSE))
 		stop("x of unexpected length")
